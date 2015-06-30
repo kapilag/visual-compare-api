1ac39f9deed55705fde3ab79a5a3aa391e411c06
kapil aggarwal <kapil.aggarwal@springer.com>
2015-06-30 07:08:41 +0000

    app.js edited online with Bitbucket Increasing size of the image

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
